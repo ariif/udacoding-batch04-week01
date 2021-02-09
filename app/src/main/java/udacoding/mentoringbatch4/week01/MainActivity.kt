eb7f12956e7fcc7a044fd9553be10f4659066197
ariif <arf.pnp.amd@gmail.com>
2021-02-09 10:29:39 +0700

    commit starter

---
+++ app/src/main/java/udacoding/mentoringbatch4/week01/MainActivity.kt
@@ -0,0 +1,11 @@
+package udacoding.mentoringbatch4.week01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
